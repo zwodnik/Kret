ec16e1086876a1f053e824092088a6f6ba397a5f
zwodnik <kamil.jarosinski@gmail.com>
2014-12-13 15:36:50 +0100

    player

---
+++ Kret/Kret/Kret.Shared/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kret
+{
+    public class Player
+    {
+        public string Name
+        {
+            set;
+            get;
+        }
+
+        public int Score
+        {
+            set;
+            get;
+        }
+
+        public int Lives
+        {
+            set;
+            get;
+        }
+
+        public Player(string name, int score) 
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public Player(string name, int score, int lives)
+        {
+            Name = name;
+            Score = score;
+            Lives = lives;
+        }
+    }
+}
---
+++ Kret/Kret/Kret.Shared/SaveReadFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Kret
+{
+    public class SaveReadFile
+    {
+        public string Uri
+        {
+            set;
+            get;
+        }
+
+        private List<Player> _players;
+
+        public SaveReadFile(string FileName)
+        {
+            _players = new List<Player>();
+            Uri = FileName;
+            _readFile();
+        }
+
+        public List<Player> Players
+        {
+            get
+            {
+                return _players;
+            }
+        }
+
+        private async Task _readFile()
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///"+Uri));
+            var stream = await file.OpenStreamForReadAsync();
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    var a = sr.ReadLine();
+                    try
+                    {
+                        string[] ret = a.Split('|');
+                        _players.Add(new Player(ret[0], int.Parse(ret[1])));
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    }
+                }
+            }
+        }
+        
+    }
+}
