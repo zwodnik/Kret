ad74c446130ba1bd925c8636262ea1f11f227d6a
kroQ <morvidkrk@gmail.com>
2014-12-13 14:54:22 +0100

    lkjhg

---
+++ Kret/Kret/Kret.Shared/Krecik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kret
+{
+    class Krecik
+    {
+        private double Height;
+        private bool isShow {get; set;}
+
+
+
+        public Krecik()
+        {
+            this.isShow = false;
+            this.Height = 100; // TRZEBA WPISAC WYSOKOSC
+            
+        }
+
+
+
+
+    }
+}
